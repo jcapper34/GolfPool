cb9961b2e37c5d828fe50b4901cac15df26a7fff
John Capper <j.capper2@gmail.com>
2021-04-11 10:53:21 -0600

    Removed print statement in pickset modal routing

--- results/routes.py
+++ results/routes.py
@@ -89,9 +89,6 @@ def get_pickset_modal(year=CURRENT_YEAR, tid=None):
         if pick.raw_pos is None:
             pick.raw_pos = 9999
 
-    for pick in pickset.picks:
-        print(pick.raw_pos)
-
     pickset_modal = get_template_attribute("modal.macro.html", "pickset_modal")
     return pickset_modal(pickset)
 
